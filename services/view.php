d6d90d198a51eb67b17c0cf1da8549bac309c4de
Хуснуллин Риналь <fredygames66@gmail.com>
2022-11-14 22:09:34 +0200

    Initial commit for project

---
+++ services/view.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/../boot.php';
+
+function view(string $path, array $variables = []): string
+{
+	if (!preg_match('/^[0-9A-Za-z\/_-]+$/', $path))
+	{
+		return 'Invalid template path';
+	}
+
+	$absolutePath = ROOT . "/views/$path.php";
+
+	if (!file_exists($absolutePath))
+	{
+		return 'Invalid template path';
+	}
+
+	extract($variables);
+
+	ob_start();
+
+	require $absolutePath;
+
+	return ob_get_clean();
+}
